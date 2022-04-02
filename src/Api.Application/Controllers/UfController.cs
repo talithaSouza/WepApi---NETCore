bee62f6b5284ed4e572d28370a5e11afc3efa241
Talitha Souza <litha06h@gmail.com>
2022-04-02 12:20:12 -0300

    implementado controller de UF

---
+++ src/Api.Application/Controllers/UfController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Intereface.Services.Uf;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UfController : ControllerBase
+    {
+        private IUfService _service;
+
+        public UfController(IUfService service)
+        {
+            _service = service;
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("{id}", Name = "GetWithId")]
+        public async Task<ActionResult> Get(Guid Id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            try
+            {
+                var result = await _service.Get(Id);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
